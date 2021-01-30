a0f6fd68ab3f7510e64477e6d784f710fcac61de
Garrett Brown <garrettpaulbrown@gmail.com>
2021-01-30 13:51:23 -0800

    Insert photos into html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     $("#formQuiz").submit(function(event) {
         event.preventDefault();
-        let vacation = $("input:radio[name=question1]:checked").val();
-        let disney = $("input:radio[name=question2]:checked").val();
-        let cool = $("input:radio[name=question3]:checked").val();
-        let dreamworks = $("input:radio[name=question4]:checked").val();
-        let emoji = $("input:radio[name=question5]:checked").val();
-        console.log(vacation, disney, cool, dreamworks, emoji);
+        const vacation = $("input:radio[name=question1]:checked").val();
+        const disney = $("input:radio[name=question2]:checked").val();
+        // const cool = $("input:radio[name=question3]:checked").val();
+        // const dreamworks = $("input:radio[name=question4]:checked").val();
+        // const emoji = $("input:radio[name=question5]:checked").val();
+    
+        if (vacation === "Greece" && disney === "Cinderella") {
+            $("#ruby").show();
+        }
+            else if (vacation === "Greece" && disney === "Mulan") {
+            $("#javascript").show(); 
+        }
+            else if (vacation === "France") {
+                $("#csharp").show();
+        }
+
     });
 });
