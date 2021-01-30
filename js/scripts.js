cceca10329db922c85fed53025032e7f7c62eefc
Garrett Brown <garrettpaulbrown@gmail.com>
2021-01-30 12:42:56 -0800

    Add JS and HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $("form#quiz").submit(function(event) {
         event.preventDefault();
-        const
+        const vacation = $("input:radio[name=question1]:checked").val();
+        const disney = $("input:radio[name=question2]:checked").val();
+        const cool = $("input:radio[name=question3]:checked").val();
+        const dreamworks = $("input:radio[name=question4]:checked").val();
+        const emoji = $("input:radio[name=question5]:checked").val();
     });
+    console.log(vacation, disney, cool, dreamworks, emoji)
 });
+
